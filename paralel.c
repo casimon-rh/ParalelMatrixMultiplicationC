169fe9018de66c557ccd3b3f2adb70c32263cdc6
Carlos Eduardo Simon Zepeda <firesstiller@gmail.com>
2017-04-22 14:57:56 -0500

    version inestable con matrices aumentadas

--- paralel.c
+++ paralel.c
@@ -7,95 +7,117 @@
 
 int main(){
 
-    int nproc = 3, i=0, status=0,j=0,k=0;
-    int ffilas = 3, fcolumnas = 4;
-    int sfilas = 4, scolumnas = 3;
+    int nproc = 2, i=0, j=0,k=0, active=1, current = 0, status=0;
+    int FilasA = 3, ColumnasA = 4;
+    int FilasB = 4, ColumnasB = 3;
     int pipefd[2],nbytes;
-    char buf;
+    pid_t pid=1,wpid;
     srand(time(NULL));
     //el primer elemento de la matriz va a ser el que indica en qué fila está
-    int **first = (int **) malloc(ffilas*sizeof(int *));
-    for(i =0; i<ffilas;i++ ){
-        first[i] = (int *) malloc(fcolumnas*sizeof(int));
+    //Alocacion e inicializacion
+    int **matriz2 = (int **) malloc(FilasA*sizeof(int *));
+    for(i =0; i<FilasA;i++ ){
+        matriz2[i] = (int *) malloc(ColumnasA*sizeof(int));
     }
-    for(i = 0; i<ffilas; i++){
-        first[i][0] = i;
-        for(j=1; j<fcolumnas; j++){
-            first[i][j]=rand()%20;
+    int * pids = (int *) malloc(nproc * sizeof(pid_t));
+    for(i = 0; i<FilasA; i++){
+        matriz2[i][0] = i;
+        for(j=1; j<ColumnasA; j++){
+            int nntmp= rand()%20;
+            matriz2[i][j]=nntmp;
         }
     }
-    int **second = (int **) malloc(sfilas*sizeof(int *));
-    for(i =0; i<sfilas;i++ ){
-        second[i] = (int *) malloc(scolumnas*sizeof(int));
+    int **matriz1 = (int **) malloc(FilasB*sizeof(int *));
+    for(i =0; i<FilasB;i++ ){
+        matriz1[i] = (int *) malloc(ColumnasB*sizeof(int));
     }
-    for(j = 0; j<scolumnas; j++){
-        second[0][j] = j;
-        for(i=1; i<sfilas; i++){
-            second[i][j]=rand()%20;
+    for(j = 0; j<ColumnasB; j++){
+        matriz1[0][j] = j;
+        for(i=1; i<FilasB; i++){
+            int ntmp= rand()%20;
+            matriz1[i][j]=ntmp;
         }
     }
-    int **third = (int **) malloc(ffilas*sizeof(int *));
-    for(i =0; i<ffilas;i++ ){
-        third[i] = (int *) malloc(fcolumnas*sizeof(int));
+    int **third = (int **) malloc(FilasA*sizeof(int *));
+    for(i =0; i<FilasA;i++ ){
+        third[i] = (int *) malloc(ColumnasA*sizeof(int));
     }
-    for(i = 0; i<ffilas; i++){
-        for(j=0; j<fcolumnas; j++){
+    for(i = 0; i<FilasA; i++){
+        for(j=0; j<ColumnasA; j++){
             third[i][j]=0;
         }
     }
-    
-    for(i =0; i<nproc; i++){
-        int *temp = malloc(fcolumnas * sizeof(int));
-        for(j=0; j<fcolumnas; j++){
+    //primer ciclo para numero de procesos **en caso de que sean igual al numero de filas
+    for(i =0; i<FilasA; i++){
+        int *temp = malloc(ColumnasA * sizeof(int));
+        for(j=0; j<ColumnasA; j++){
             temp[j]=0;
         }
-        pipe(pipefd);
-        pid_t pid = fork();
-        if(pid==0){//codigo hijo     
-            temp[0]= i;
-            for(j=1; j<fcolumnas; j++){
-                for(k=1; k<sfilas; k++){
-                    temp[j]+=first[k-1][i+1]*second[j][k-1];
+        if(active <= nproc&& pid>0){
+            pipe(pipefd);
+            pid = fork();
+            pids[active-1] = pid;
+            current = active;
+            active++;
+        }
+        if(pid==0){//codigo hijo
+        /**************************************************************/
+            printf("\t%d %% %d = %d igual %d %% %d = %d desde %d\n",(i),nproc,(i)%nproc,(current),nproc,(current)%nproc,getpid());
+            if(((i)%nproc)!=((current)%nproc)){//asignación//
+        /**************************************************************/
+                temp[0]= i;
+                for(j=1; j<ColumnasA; j++){
+                    for(k=1; k<FilasB; k++){
+                        temp[j]+=matriz2[k-1][i+1]*matriz1[j][k-1];
+                    }
                 }
+                close(pipefd[0]);
+                write(pipefd[1],temp,4*sizeof(int));
+                printf("\tavienta un pipe de i=%d desde %d\n",temp[0],getpid());
             }
-            close(pipefd[0]);
-            write(pipefd[1],temp,4*sizeof(int));
-            exit(0);
+            //exit(0);
         }else if(pid>0){//codigo padre
+            //while((wpid = wait(&status)) >0);            
             close(pipefd[1]);
             nbytes = read(pipefd[0],temp,4*sizeof(int));
-            wait(NULL); 
             third[temp[0]][0] = temp[0];
-            for(j=1; j<fcolumnas; j++){
+            for(j=1; j<ColumnasA; j++){
                 third[j-1][temp[0]+1] = temp[j];
             }
-            if(i==(nproc-1)){
-                for(i = 0; i<ffilas; i++){
-                    for(j=0; j<fcolumnas; j++){
-                        printf("|%d| ",first[i][j]);
+            if(i==(FilasA-1)){
+                int o = i;
+                printf("_________________________________________________\n");                
+                for(i = 1; i<FilasB; i++){
+                    for(j=0; j<ColumnasB; j++){
+                        printf("|\t%d\t",matriz1[i][j]);
                     }
-                    printf("\n");
+                    printf("|\n");
                 }
-                printf("\n");
-                for(i = 0; i<sfilas; i++){
-                    for(j=0; j<scolumnas; j++){
-                        printf("|%d| ",second[i][j]);
+                printf("_________________________________________________\n");
+                for(i = 0; i<FilasA; i++){
+                    for(j=1; j<ColumnasA; j++){
+                        printf("|\t%d\t",matriz2[i][j]);
                     }
-                    printf("\n");
+                    printf("|\n");
                 }
-                printf("\n");
-                for(i = 0; i<ffilas; i++){
-                    for(j=0; j<fcolumnas; j++){
-                        printf("|\t%d\t|",third[i][j]);
+                printf("_________________________________________________\n");
+                for(i = 0; i<FilasA; i++){
+                    for(j=1; j<ColumnasA; j++){
+                        printf("|\t%d\t",third[i][j]);
                     }
-                    printf("\n");
+                    printf("|\n");
                 }
-
+                printf("_________________________________________________\n");
+                i = o;
+            for(j=0 ; j< nproc; j++){
+                waitpid(pids[j],NULL,0);
+            }
             return 0;
             }
         }else{
             printf("Error");
             exit(1);
         }
-    }    
+    }
+    exit(0);    
 }
